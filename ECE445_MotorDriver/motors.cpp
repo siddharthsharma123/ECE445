a0c716cf6316a3be4d158a4a8637913590496b8d
Shouri Addepally <60534857+shouriadde23@users.noreply.github.com>
2022-11-05 14:04:57 -0700

    updating motors.cpp

--- ECE445_MotorDriver/motors.cpp
+++ ECE445_MotorDriver/motors.cpp
@@ -1,27 +1,63 @@
 #include <Arduino.h> 
-#include "./motors.h"
+#include "./motor_Driver.h"
 
 using namespace std; 
 
 Motors::Motors(int LeftMotorForward, int LeftMotorBackward, int RightMotorForward, int RightMotorBackward) {
-    LeftMotorForward = LeftMotorForward;
-    LeftMotorBackward = LeftMotorBackward;
-    RightMotorForward = RightMotorForward;
-    RightMotorBackward = RightMotorBackward;
+    this->LeftMotorForward = LeftMotorForward;
+    this->LeftMotorBackward = LeftMotorBackward;
+    this->RightMotorForward = RightMotorForward;
+    this->RightMotorBackward = RightMotorBackward;
 }
 
 void Motors::moveForward() {
-    
-}  
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+  
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorBackward, LOW);
+}
+
+void Motors::moveBackward() {
+    digitalWrite(LeftMotorBackward, HIGH);
+    digitalWrite(RightMotorBackward, HIGH);
+  
+    digitalWrite(LeftMotorForward, LOW);
+    digitalWrite(RightMotorForward, LOW);
+}
 
 void Motors::turnRight(){
-    
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorBackward, HIGH);
+  
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorForward, LOW);
+  
+    delay(500);
+  
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+  
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorBackward, LOW);
 }
 
 void Motors::turnLeft(){
-    
+    digitalWrite(LeftMotorBackward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+  
+    digitalWrite(LeftMotorForward, LOW);
+    digitalWrite(RightMotorBackward, LOW);
+
+    delay(500);
+  
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+  
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorBackward, LOW);
 }
 
 int Motors::getLeftMotorForward() {
-    return this.LeftMotorForward;
+    return this->LeftMotorForward;
 }

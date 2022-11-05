c09206f5f42a81507eb7426b98acd20edc2881a9
Shouri Addepally <60534857+shouriadde23@users.noreply.github.com>
2022-11-05 14:03:36 -0700

    updating ino file for simple motor driver test

--- ECE445_MotorDriver/ECE445_MotorDriver.ino
+++ ECE445_MotorDriver/ECE445_MotorDriver.ino
@@ -1,12 +1,14 @@
-#include "./motors.cpp" 
+#include "data/motor_driver.cpp"
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
 }
 Motors motors(4,5,6,7); 
-Serial.print(motors.getLeftMotorForward())
+
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.print(motors.getLeftMotorForward());
+  //Serial.print(0);
 }
